037ca9881f5edd7315cf95110473ced9c64e5045
B Gray <strider_3029@yahoo.com>
2015-04-28 23:38:31 +1000

    Initial Commit

---
+++ NModM/NModM.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace NModM
+{
+    class NModM
+    {
+        static int Main(string[] args)
+        {
+            // Check the argument and file exists
+            if (args.Length == 0 || !File.Exists(args[0]))
+            {
+                Console.Write("You failed to specify the file to read, or entered a non existant file path.\nPlease try again with the file name as the first argument.");
+                return 0;
+            }
+
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (null == line) continue;
+
+                    if ("" == line) continue;
+
+                    string[] numberBlocks = line.Split(',');
+
+                    if (numberBlocks.Length != 2) continue;
+
+                    int n = Convert.ToInt32(numberBlocks[0]);
+                    int m = Convert.ToInt32(numberBlocks[1]);
+
+                    PrintNModM(n, m);
+                }
+            }
+
+            return 0;
+        }
+
+        static void PrintNModM(int n, int m)
+        {
+            if (m == 0) return;
+
+            int quotient = n / m;
+            int product = quotient * m;
+            int nModm = n - product;
+
+            Console.WriteLine(nModm);
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+C# code sample
+
+CodeEval: N Mod M
+URL: https://www.codeeval.com/open_challenges/62/
+
+Given two integers N and M, calculate N Mod M (without using any inbuilt modulus operator).
